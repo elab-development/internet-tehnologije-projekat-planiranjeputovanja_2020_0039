5a76c184d48456e6b77594584157801a7bf41671
Nale1409 <nj20200194@student.fon.bg.ac.rs>
2023-12-06 14:54:38 +0100

    modeli

---
+++ projekat/database/migrations/2023_12_06_134226_create_attractions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attractions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('country_id');
+            $table->text('address');
+            $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attractions');
+    }
+};
