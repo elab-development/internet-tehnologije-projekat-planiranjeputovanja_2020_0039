592cc04d6c56b2eb5821891ac77a083b5e11badb
Nale1409 <nj20200194@student.fon.bg.ac.rs>
2024-01-17 12:48:49 +0100

    PromenaLoznke

--- planning_website/src/components/RegisterForm.js
+++ planning_website/src/components/RegisterForm.js
@@ -31,7 +31,7 @@ const RegisterForm = ({ onSubmit }) => {
         formData.append(key, value);
       }
   
-      const response = await fetch('http://localhost:8000/api/register', {
+      const response = await fetch('http://127.0.0.1:8000/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
